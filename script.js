9e57f1b7dc1a966ecee9c52f719c1f2ec03938d5
Pietro Bucker <pietro_177@hotmail.com>
2022-10-04 16:07:10 -0300

    requisitos1 ao 4

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 window.onload = function () {
-const buttonRandom = document.querySelector('#button-random-color');
+  const buttonRandom = document.querySelector('#button-random-color');
 
-function randomColorPalette (){
-  const colorBox = document.querySelectorAll('.color');
-    for (let index = 1; index < colorBox.length; index += 1 ){
-      const randomR = Math.floor(Math.random()*255+1);
-      const randomG = Math.floor(Math.random()*255+1);
-      const randomB = Math.floor(Math.random()*255+1);
-      if(randomR !== randomG !== randomB){
-      colorBox[index].style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+  function randomColorPalette() {
+    const colorBox = document.querySelectorAll('.color');
+    for (let index = 1; index < colorBox.length; index += 1) {
+      const randomR = Math.floor(Math.random() * 255 + 1);
+      const randomG = Math.floor(Math.random() * 255 + 1);
+      const randomB = Math.floor(Math.random() * 255 + 1);
+      if ((randomR !== randomG) !== randomB) {
+        colorBox[
+          index
+        ].style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
       }
     }
-}
-buttonRandom.addEventListener('click', randomColorPalette)
-
-
-
-
-
-colorPalette();
+  }
+  buttonRandom.addEventListener('click', randomColorPalette);
+  colorPalette();
 };
 
 function colorPalette() {
   const color = document.querySelectorAll('.color');
-  console.log(color);
   color[0].style.backgroundColor = 'black';
   color[1].style.backgroundColor = 'red';
   color[2].style.backgroundColor = 'blue';
