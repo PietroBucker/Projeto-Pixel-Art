5e19beae944d8a0e72149929840f2a748bf81a09
Pietro Bucker <pietro_177@hotmail.com>
2022-10-04 18:17:44 -0300

    requisito 6 e 7 feito

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 window.onload = function () {
-  const buttonRandom = document.querySelector('#button-random-color');
-
-  function randomColorPalette() {
-    const colorBox = document.querySelectorAll('.color');
-    for (let index = 1; index < colorBox.length; index += 1) {
-      const randomR = Math.floor(Math.random() * 255 + 1);
-      const randomG = Math.floor(Math.random() * 255 + 1);
-      const randomB = Math.floor(Math.random() * 255 + 1);
-      if ((randomR !== randomG) !== randomB) {
-        colorBox[
-          index
-        ].style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
-      }
-    }
-  }
   buttonRandom.addEventListener('click', randomColorPalette);
   buttonRandom.addEventListener('click', setColorStorage);
 
+  createPixels();
   colorPalette();
   getColorStorage();
 };
 const colors = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+const buttonRandom = document.querySelector('#button-random-color');
 
 function colorPalette() {
   const color = document.querySelectorAll('.color');
@@ -37,5 +25,38 @@ function getColorStorage() {
   const getColor = localStorage.getItem('colorPalette');
   if (getColor !== null) {
     colors.innerHTML = JSON.parse(getColor);
+  }
+}
+function pixelStyle(param) {
+  param.className = 'pixel';
+  param.style.border = '1px solid black';
+  param.style.width = '40px';
+  param.style.height = '40px';
+  param.style.backgroundColor = 'white';
+  param.style.display = 'inline-block';
+  param.style.margin = '0';
+}
+function randomColorPalette() {
+  const colorBox = document.querySelectorAll('.color');
+  for (let index = 1; index < colorBox.length; index += 1) {
+    const randomR = Math.floor(Math.random() * 255 + 1);
+    const randomG = Math.floor(Math.random() * 255 + 1);
+    const randomB = Math.floor(Math.random() * 255 + 1);
+    if ((randomR !== randomG) !== randomB) {
+      colorBox[
+        index
+      ].style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+    }
+  }
+}
+function createPixels() {
+  for (let i = 0; i < 5; i += 1) {
+    for (let j = 0; j < 5; j += 1) {
+      const creatPixel = document.createElement('div');
+      pixelStyle(creatPixel);
+      pixelBoard.appendChild(creatPixel);
+    }
+    const br = document.createElement('br');
+    pixelBoard.appendChild(br);
   }
 }
